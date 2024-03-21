e9618e3b04b204925452fcfd428d962cbed22cf3
Gustavoxyz <gustavosxyz@gmail.com>
2024-03-20 19:57:26 -0500

    ambiente

---
+++ Application/Ambiente/Queries/AmbienteDTO.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Mappings;
+using Application.PersonalSoporte.Queries;
+using AutoMapper;
+using Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Ambiente.Queries
+{
+    public class AmbienteDTO: IMapFrom<AmbienteEntity>
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Vigente { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<AmbienteEntity, AmbienteDTO>();
+        }
+    }
+}
---
+++ Application/Ambiente/Queries/ListarAmbienteQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces.Repositorios;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Ambiente.Queries
+{
+    public class ListarAmbienteQueryHandler : IRequestHandler<ListarAmbienteQuery, IEnumerable<AmbienteDTO>>
+    {
+        private readonly IAmbienteRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ListarAmbienteQueryHandler(IAmbienteRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<AmbienteDTO>> Handle(ListarAmbienteQuery request, CancellationToken cancellationToken)
+        {
+            var lista = await _repository.ListarAmbiente();
+            return _mapper.Map<IEnumerable<AmbienteDTO>>(lista);
+        }
+    }
+}
